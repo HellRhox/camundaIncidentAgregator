e8ad963495d94fed49b4ef2b03881897123cbc73
Yannick Bayard <yannick.bayard@inexio.net>
2023-07-26 15:07:43 +0200

    corrected some copy and past error

--- pkg/utils/timeFormat/timeFormat.go
+++ pkg/utils/timeFormat/timeFormat.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetTimeFormatForRest(month int, day int) (string, string) {
-	startDay := strings.Replace(time.Now().AddDate(0, month, day).Format("2006-01-02T15:04:05.000-0700"), "+", "-", 1)
+	startDay := strings.Replace(time.Now().AddDate(0, -month, -day).Format("2006-01-02T15:04:05.000-0700"), "+", "-", 1)
 	endDay := strings.Replace(time.Now().Format("2006-01-02T15:04:05.000-0700"), "+", "-", 1)
 
 	return startDay, endDay
